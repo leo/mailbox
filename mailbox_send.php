58551374d997e7582e278a3872339481a1c55020
Leonard <lmprht@users.noreply.github.com>
2014-11-15 21:37:30 +0100

    Version 4
    See commit on main file for more info.

--- mailbox_send.php
+++ mailbox_send.php
@@ -1,51 +1,38 @@
 <?php
-
+	
 	/*
-		Mailbox 3 - Replacing mailto-links with contact-forms.
-		Copyright by Leonard Lamprecht - //github.com/leo/mailbox
-		License: http://git.io/c9_RLA
+		Mailbox 4 - A contact-form for the new web.
+		Made by Leonard Lamprecht - //github.com/leo/mailbox
 	*/
-
-
-	if(isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
 	
-		$to = filter_var($_POST['to'], FILTER_SANITIZE_STRING);
-			
-		$cc = filter_var($_POST['cc'], FILTER_SANITIZE_STRING);
-		$bcc = filter_var($_POST['bcc'], FILTER_SANITIZE_STRING);
-	
-		$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-		$address = filter_var($_POST['address'], FILTER_SANITIZE_STRING);
-		$subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
-		$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
-
-		$sent = email($to, $cc, $bcc, $name, $address, $subject, $message);
+	if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) ) {
 		
-		if ($sent) {
-			echo 'success';
-		} else {
-			echo 'error';
+		foreach( $_POST as $key => $content ) {
+			$args[$key] = filter_var( $_POST[$key], FILTER_SANITIZE_STRING );
 		}
-
-		return;
-	}
-
-	function email($to, $cc, $bcc, $from_name, $from_email, $subject, $message){
-
-		$headers   = array();
+		
+		$headers = array();
 		$headers[] = "MIME-Version: 1.0";
 		$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-		$headers[] = "From: {$from_name} <{$from_email}>";
-		if ($cc !== 'false') {
-			$headers[] = "Cc: {$cc}";
+		$headers[] = "Return-Path: {$args['adress']}";
+		$headers[] = "From: {$args['name']} <{$args['adress']}>";
+		
+		if( $args['cc'] !== null ) {
+			$headers[] = "Cc: {$args['cc']}";
 		}
-		if ($bcc !== 'false') {
-			$headers[] = "Bcc: {$bcc}";
+		
+		if( $args['bcc'] !== null ) {
+			$headers[] = "Bcc: {$args['bcc']}";
 		}
-		$headers[] = "Subject: {$subject}";
-
+		
+		if( mail( $args['to'], $args['subject'], $args['body'], implode( "\r\n", $headers ) ) ) {
+			echo 1;
+		} else {
+			echo 0;
+		}
+		
+		return;
 	
-		if( mail($to, $subject, $message, implode("\r\n", $headers)) ) return true; 
 	}
 
 ?>
